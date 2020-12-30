de037afb3fdc7051af5213ca27f79f00772b7874
MillarayM <millaray.sw@gmail.com>
2020-12-30 14:46:39 -0300

    consulta de ingreso

---
+++ consultas.js
@@ -0,0 +1,27 @@
+// funcion, consulta sql . agregar usuario 1 para en la tabla usuarios
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  password: "postgres",
+  port: 5432,
+  database: "softlife",
+});
+
+const agregarUsuario = async (datos) => {
+  const consultaIngreso = {
+    text: "INSERT INTO usuarios (email, password) VALUES ($1, $2) RETURNING *;",
+    values: datos,
+  };
+  try {
+    const resultado = await pool.query(consultaIngreso);
+    console.log ( resultado.rows)
+    return resultado.rows
+  } catch (error) {
+    console.log(error.code);
+  }
+};
+agregarUsuario(["fffffdddjsj@jsjs.cl", "dhdhd"]);
+
