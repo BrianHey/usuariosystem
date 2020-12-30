9d8579cfdc55234d8f6d47ed5c3756af4bf49c31
ElpolMorris <pmorales.contacto@gmail.com>
2020-12-30 14:30:50 -0300

    ruta raiz usuarios

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const fs = require("fs")
 const http = require("http")
 
 http.createServer( (req, res) => {
-    
+    if(req.url == '/' && req.method == 'GET'){
+        res.setHeader('content-type','text/html')
+        const html = fs.readFileSync('index.html','utf8')
+        res.end(html)
+    }
     
 }).listen(3000)
